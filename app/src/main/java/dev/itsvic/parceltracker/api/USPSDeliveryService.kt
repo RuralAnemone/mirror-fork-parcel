dc4af947e0f6c828c48b9d59daed7ec1f0b96bdb
Isaiah W <isaiah.wallerstedt@proton.me>
2025-04-08 21:12:00 -0500

    add retrofit

--- app/src/main/java/dev/itsvic/parceltracker/api/USPSDeliveryService.kt
+++ app/src/main/java/dev/itsvic/parceltracker/api/USPSDeliveryService.kt
@@ -11,6 +11,7 @@ import android.util.Log
 import kotlinx.coroutines.ExperimentalForInheritanceCoroutinesApi
 import org.json.JSONObject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import retrofit2.Retrofit
 
 object USPSDeliveryService : DeliveryService {
     override val nameResource: Int = R.string.service_usps
@@ -18,6 +19,7 @@ object USPSDeliveryService : DeliveryService {
     override val requiresPostCode: Boolean = false
 
     override suspend fun getParcel(trackingId: String, postCode: String?): Parcel {
+
         return Parcel(
             trackingId,
             "history",
@@ -25,6 +27,9 @@ object USPSDeliveryService : DeliveryService {
             "metadata"
         )
     }
+
+    private val retrofit = Retrofit.Builder().baseUrl("https://api.usps.com/").client(api_client)
+        .addConverterFactory(api_factory).build()
 
     @OptIn(ExperimentalCoroutinesApi::class)
     private suspend fun getOauthToken(clientId: String, clientSecret: String): String {
@@ -49,7 +54,7 @@ object USPSDeliveryService : DeliveryService {
                 Log.d("USPS", "Got Oauth2 token! $jsonWebToken")
                 // TODO: verify access token with provided public key
                 // if the USPS gets hacked we may have bigger problems on our hands than inaccurate package tracking
-            } else Log.d("USPS", "Couldn't get Oauth2 token ):")
+            } else Log.d("USPS", "Couldn't get Oauth2 token ): response code was ${response.code}")
             return jsonWebToken
         }
     }
